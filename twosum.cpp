52fb4c7393495c2ce7bbc659a30932d265aae4d5
jwerenski <68121538+jwerenski@users.noreply.github.com>
2021-06-28 15:27:36 -0400

    completed twosum challenge

--- twosum.cpp
+++ twosum.cpp
@@ -1,13 +1,43 @@
+#include <cassert>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <iterator>
+#include <map>
+
+using std::map;
+using std::vector;
+using std::cout;
+using std::endl;
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
+
+        vector<int> soln = {-1,-1};
+
+        for(int i = 0; i < nums.size(); i++) {
+            for(int j = i + 1; j < nums.size(); j++){
+                if(target - nums.at(i) == nums.at(j)){
+                    soln = {i,j};
+                }
+            }
+        }
+        return soln;
     }
 };
 
 
 int main() {
 
+    Solution mysoln;
+
+    vector<int> nums = {1,3,5,7};
+    int target = 8;
+    vector<int> myvec = mysoln.twoSum(nums,target);
+
+    cout << myvec[0] << " " << myvec[1] << endl;
 
 
     return 0;
